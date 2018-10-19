413b990f085581753c56dadf81fae58cfbb76d95
Bigger <vagrant@homestead>
2018-10-19 13:27:05 +0000

    finsh layouts and route

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>@yield('title', 'Sample')</title>
+    <meta charset="UTF-8">
+    <title>@yield('title', 'Sample App')- Laravel 入门教程</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+<div class="container">
+    @yield('content')
+    @include('layouts._footer')
+</div>
 </body>
 </html>
