e43bd06ed48b712d5e2fc50404445a91d5c34311
Bigger <1377293017@qq.com>
2018-10-20 07:25:02 +0000

    fix helpers.php code issue

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,7 +4,7 @@ function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
         # code...
-        $url = parse_url(getenv('IS_IN_HEROKU'));
+        $url = parse_url(getenv('DATABASE_URL'));
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
